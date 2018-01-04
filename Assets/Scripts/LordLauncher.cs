3b7702ba74cf901ed5de850bf0c42adb2f0c8e5a
Dakota Ling <ourthinking@gmail.com>
2018-01-04 01:00:17 -0500

    initial commit
    Committing project as used in the final render for the 12 days short.

---
+++ Assets/Scripts/LordLauncher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LordLauncher : MonoBehaviour {
+
+	Rigidbody2D body;
+
+	public float windupTime;
+	public Vector2 windupForce;
+
+	bool windingUp = false;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+
+		StartCoroutine(WindUpAndRelease());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (windingUp) {
+			body.AddForce(windupForce, ForceMode2D.Impulse);
+		}
+	}
+
+	IEnumerator WindUpAndRelease() {
+
+		windingUp = true;
+		body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+
+		yield return new WaitForSeconds(windupTime);
+
+		windingUp = false;
+		body.constraints = RigidbodyConstraints2D.None;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Leaping Lords
+
+Unity project for my contribution to the Primal 12 Days short. Hit play and watch the ragdoll lords fly. All art is public domain.
+
+Scene was recorded in slow motion and brought into After Effects for time remapping.
+
+[Lords a Leaping video](https://drive.google.com/file/d/1V9_i005riyIZCD9sIzLYo-xMnSQ7Y2Ow/view?usp=sharing)
+
+[12 Days short](https://www.instagram.com/p/BdIyYf4BjNv/?hl=en&taken-by=primal_screen)
